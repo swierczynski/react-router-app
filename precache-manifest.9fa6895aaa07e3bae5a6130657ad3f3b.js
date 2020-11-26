ee520667d4ed64498da87d893aaae0b48c0b17e8
Kamil Swierczynski <swierk1997@gmail.com>
2020-11-26 13:29:00 +0100

    Updates

---
+++ precache-manifest.9fa6895aaa07e3bae5a6130657ad3f3b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "98582788cc3080a7ce66",
+    "url": "/react-router-app/static/css/main.50004c2d.chunk.css"
+  },
+  {
+    "revision": "98582788cc3080a7ce66",
+    "url": "/react-router-app/static/js/main.b0e000b2.chunk.js"
+  },
+  {
+    "revision": "80e81a85950596580de4",
+    "url": "/react-router-app/static/js/runtime~main.1f157a88.js"
+  },
+  {
+    "revision": "42afcd39b86ca19477da",
+    "url": "/react-router-app/static/js/2.e1f51481.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-router-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-router-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-router-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "fc777322499d590ab51aeb87cd74d2a0",
+    "url": "/react-router-app/static/media/errorpage.fc777322.jpeg"
+  },
+  {
+    "revision": "f66e82202ecb209f3ee54bd0301756de",
+    "url": "/react-router-app/index.html"
+  }
+];
